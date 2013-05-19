2f99f5736b34667830e7886c2cbc2e157514777d
wevaldivieso <wevaldivieso@gmail.com>
2013-05-18 23:53:17 -0500

    Revert "creación del proyecto"
    This reverts commit 8c262f280b9bcbe9511080780d0bb136a0af2579.

--- conciertos/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Conciertos::Application.config.secret_token = '00a9748ee083ea6c5e5faeb9c5cef4c40657c8fe8950e9b4ef8245882ff0ab17f984ba3a818d6c53090a3b8e22d85f444f21103c087f6b5e3819d45da696f598'
