c7a6dc231345543c720230287229289f1c7b3d8b
Pierre RB <oprbguitar@gmail.com>
2013-05-19 23:50:56 -0500

    Investigando vistas
    Investigando vistas

--- ConciertoUPC/config/routes.rb
+++ ConciertoUPC/config/routes.rb
@@ -1,18 +1,11 @@
 ConciertoUPC::Application.routes.draw do
-  root :to => 'users#index'
+  get "welcome/index"
+
 
   resources :users
-
-
   resources :bands
-
-
   resources :styles
-
-
   resources :locals
-
-
   resources :concerts
 
 
@@ -65,7 +58,7 @@ ConciertoUPC::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
