e6755157fd32c833346d7f661884bfc4b78d7187
wevaldivieso <wevaldivieso@gmail.com>
2013-06-05 23:39:17 -0500

    Ultima version.
    Se corrigen errores en el migrate

--- ConciertoUPC/db/migrate/20130521205614_drop_styles_table.rb
+++
@@ -1,8 +0,0 @@
-class DropStylesTable < ActiveRecord::Migration
-  def up
-  		drop_table :Styles
-  end
-
-  def down
-  end
-end
--- ConciertoUPC/db/migrate/20130521210644_create_styles.rb
+++
@@ -1,9 +0,0 @@
-class CreateStyles < ActiveRecord::Migration
-  def change
-    create_table :styles do |t|
-      t.string :nombre
-
-      t.timestamps
-    end
-  end
-end
